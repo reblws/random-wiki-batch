dd71a679337d9d9531a91aa17daf1a3cca0652ae
Enrico B <rflnrclws@protonmail.com>
2017-06-07 10:18:29 -0400

    promisify

--- src/index.js
+++ src/index.js
@@ -1,27 +1,43 @@
 const got = require('request-promise')
 
-const WIKI_ENDPOINT = 'https://en.wikipedia.org/w/api.php?';
-const format = 'format=json';
-const action = 'action=query';
-const list = 'generator=random';
-const rnLimit = 'grnlimit=10';
-const namespace = 'grnnamespace=0';
-const props = 'prop=revisions&rvprop=content'
+let test = randomWikiBatch(10);
 
-const urlToRequest = `
-  ${WIKI_ENDPOINT}${format}&${action}&${list}&${rnLimit}&${namespace}&${props}`;
+function randomWikiBatch(numOfPagesWanted) {
+    const WIKI_ENDPOINT = 'https://en.wikipedia.org/w/api.php?';
+    const format = 'format=json';
+    const action = 'action=query';
+    const list = 'generator=random';
+    const namespace = 'grnnamespace=0';
+    const props = 'prop=revisions&rvprop=content'
+    const rnLimit = num => `grnlimit=${num}`;
+    const urlToRequest = rnLimit => (`
+      ${WIKI_ENDPOINT}${format}&${action}&${list}&${rnLimit}&${namespace}&${props}
+    `);
 
+    // Can only make max of 10 requests at a time
+    const requestsToMake = Math.floor(numOfPagesWanted / 10);
+    const numOfPagesOnLastRequest = numOfPagesWanted % 10;
+    let wikiPromises = Array(requestsToMake).fill(10);
+    if (numOfPagesOnLastRequest !== 0) {
+      wikiPromises.push(numOfPagesOnLastRequest);
+    }
 
-const parseResponse = response => JSON.parse(response);
+    wikiPromises = wikiPromises.map(pagesToRequest => {
+      const numOfArticlesOnRequest = rnLimit(pagesToRequest);
+      const url = urlToRequest(numOfArticlesOnRequest);
+      return Promise.resolve(got(url))
+        .then(response => JSON.parse(response))
+        .then(getRandomPagesArray);
+    });
 
-function getRandomBatch() {
-  got(urlToRequest).then(response => handleWikiResponse(response));
+    wikiPromises = Promise.all(wikiPromises);
+  return new Promise((fulfill, reject) => {
+    fulfill(wikiPromises);
+  })
 }
 
-function handleWikiResponse(response) {
-  const randomPages = parseResponse(response).query.pages;
-  const randomPagesArray = Object.keys(randomPages).map(key => randomPages[key]);
-  console.log(randomPagesArray);
-  // We have to handle this array and push it to a global obj i guess
+function getRandomPagesArray(json) {
+  const pages = json.query.pages;
+  const randomPagesArray = Object.keys(pages).map(key => pages[key]);
   return randomPagesArray;
 }
