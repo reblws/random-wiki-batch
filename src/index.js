964ca86182c93d7d6904273d14cb27f1f3c2a073
Enrico B <rflnrclws@protonmail.com>
2017-06-10 16:45:36 -0400

    Refactor for options

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const got = require('got');
 
+const DEFAULTS = {
+  language: 'en',
+  pagesWanted: 10,
+}
+
 function getRandomPagesArray(json) {
   const pages = json.query.pages;
   const randomPagesArray = Object.keys(pages).map(key => pages[key]);
   return randomPagesArray;
 }
 
-function constructRequestURL(pagesWanted) {
+function makeRequestURL(pagesWanted, options) {
   const WIKI_ENDPOINT = 'https://en.wikipedia.org/w/api.php?';
   const format = 'format=json';
   const action = 'action=query';
@@ -14,27 +19,54 @@ function constructRequestURL(pagesWanted) {
   const namespace = 'grnnamespace=0';
   const props = 'prop=revisions&rvprop=content'
   const rnLimit = `grnlimit=${pagesWanted}`;
-  const urlToRequest = (`
+  const urlToRequest = `
     ${WIKI_ENDPOINT}${format}&${action}&${list}&${rnLimit}&${namespace}&${props}
-  `);
+  `;
 
   return urlToRequest;
 }
 
-function randomWikiBatch(numOfPagesWanted = 10) {
+function fillDefaultOptions(userOptions) {
+  const userOptionKeys = Object.keys(userOptions);
+  if (userOptionKeys.length === 0)
+    return DEFAULTS;
+
+  const fillDefaults = (allOptions, option) => {
+    const newOption = {};
+    newOption[option] = DEFAULTS[option];
+    return Object.assign({}, allOptions, newOption);
+  };
+
+  return Object.keys(DEFAULTS)
+    .filter(option => !userOptionKeys.includes(option))
+    .reduce(fillDefaults, userOptionKeys);
+}
+
+function getRequests(numOfPagesWanted) {
+  return {
+    totalRequests: Math.floor(numOfPagesWanted / 10),
+    pagesOnLastRequest: numOfPagesWanted % 10,
+  };
+}
+
+function requestPromise(pagesToRequest) {
+  const url = makeRequestURL(pagesToRequest, options);
+  return Promise.resolve(got(url))
+    .then(response => JSON.parse(response.body))
+    .then(getRandomPagesArray);
+}
+
+
+function randomWikiBatch(numOfPagesWanted = 10, userOptions = {}) {
   // Can only make max of 10 requests at a time
-  const requestsToMake = Math.floor(numOfPagesWanted / 10);
-  const numOfPagesOnLastRequest = numOfPagesWanted % 10;
-  let wikiPromises = Array(requestsToMake).fill(10);
+  const options = fillDefaultOptions(userOptions);
+  const { totalRequests, pagesOnLastRequest } = getRequests(numOfPagesWanted);
+
+  let wikiPromises = Array(totalRequests).fill(10);
   if (numOfPagesOnLastRequest !== 0) {
     wikiPromises.push(numOfPagesOnLastRequest);
   }
-  wikiPromises = wikiPromises.map(pagesToRequest => {
-    const url = constructRequestURL(pagesToRequest);
-    return Promise.resolve(got(url))
-      .then(response => JSON.parse(response.body))
-      .then(getRandomPagesArray);
-  });
+  wikiPromises = wikiPromises.map(requestPromise);
   return Promise.all(wikiPromises)
     .then(articleBatch => [].concat.apply([], articleBatch));
 }
